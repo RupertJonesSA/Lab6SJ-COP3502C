0a615a260d99909e1c876f3fdd659f1791dff2f7
RupertJonesSA <145080727+RupertJonesSA@users.noreply.github.com>
2023-10-25 15:27:55 -0400

    Sami's code

---
+++ lab6encoder.py
@@ -0,0 +1,37 @@
+"""
+Author - Sami Al-Jamal
+"""
+
+
+def encoder(password):
+    new_pass = ""
+    for char in password:
+        new_pass += str(int(char) + 3)
+    return new_pass
+
+
+def main():
+    encode_pass = ""
+    decode_pass = ""
+    while True:
+        print("Menu\n-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit\n")
+        option = int(input("Please enter an option: "))
+
+        if option == 1:
+            password = input("Please enter your password to encode: ")
+            encode_pass = encoder(password)
+            print(encode_pass)
+            print("Your password has been encoded and stored!")
+        elif option == 2:
+            """
+            Add decode function here
+            """
+        elif option == 3:
+            break
+
+
+if __name__ == "__main__":
+    main()
